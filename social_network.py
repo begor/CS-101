82c285a13b115b162ec541d31d3afa5ed5f3478c
Biriukov Egor <birykovegor@gmail.com>
2016-01-04 20:07:27 +0300

    Add new user procedure

--- social_network.py
+++ social_network.py
@@ -111,5 +111,29 @@ def add_connection(network, user_A, user_B):
 
     return network
 
+
+# ----------------------------------------------------------------------------- 
+# add_new_user(network, user, games): 
+#   Creates a new user profile and adds that user to the network, along with
+#   any game preferences specified in games. Assume that the user has no 
+#   connections to begin with.
+# 
+# Arguments:
+#   network: the gamer network data structure
+#   user:    a string containing the name of the user to be added to the network
+#   games:   a list of strings containing the user's favorite games, e.g.:
+#            ['Ninja Hamsters', 'Super Mushroom Man', 'Dinosaur Diner']
+#
+# Return: 
+#   The updated network with the new user and game preferences added. The new user 
+#   should have no connections.
+#   - If the user already exists in network, return network *UNCHANGED* (do not change
+#     the user's game preferences)
+def add_new_user(network, user, games):
+    if user not in network:
+        network[user] = {'connections': [], 'games': games}
+
+    return network
+
         
         
